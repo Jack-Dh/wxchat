c75b95803d391ab0c19be27eed7e4577727d8ad0
wechantCli <897268593@QQcom>
2018-12-17 20:42:06 +0800

    微信端vue

--- vue.config.js
+++ vue.config.js
@@ -5,9 +5,9 @@ module.exports={
     assetsDir:'assets',//静态资源目录（js.css.fonts，img）
     lintOnSave:false,//是否开启eslintao保存检测，有效值（true||false||error）
     devServer:{
-        open:false,//是否自动开启浏览器
-        host:'127.0.0.1',//配置主机名字
-        port:'8088',//配置端口号
+        // open:false,//是否自动开启浏览器
+        // host:'127.0.0.1',//配置主机名字
+        // port:'8088',//配置端口号
         https:false,//最好使用falase
         hotOnly:false,//热更新
         proxy:{
